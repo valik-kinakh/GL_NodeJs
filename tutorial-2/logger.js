07a23f455b9017c1b113542436dffe8c59023160
Valik Kinakh <valik.kinah@gmail.com>
2021-08-06 10:14:54 +0300

    task 2 done

---
+++ tutorial-2/logger.js
@@ -0,0 +1,11 @@
+function printMessages(action)
+{
+    if (action)
+    {
+        console.log("Everything is correct:)");
+    }else
+    {
+        console.error("Something gone wrong:(");
+    }
+}
+module.exports=printMessages;
